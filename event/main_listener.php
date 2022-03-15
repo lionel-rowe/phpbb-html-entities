5b7b9d395f4e657d4ad93d3b6440fdea07d9cfd9
lionel-rowe <lionel.rowe@gmail.com>
2022-03-15 21:00:25 +0000

    Account for non-code tags inside code tags

--- event/main_listener.php
+++ event/main_listener.php
@@ -20,9 +20,9 @@ class main_listener implements EventSubscriberInterface
 {
 	static public function getSubscribedEvents()
 	{
-		return array(
+		return [
 			'core.text_formatter_s9e_render_after' 		=> 'render_html_entities',
-		);
+		];
 	}
 
 	/* @var \phpbb\language\language */
@@ -30,8 +30,6 @@ class main_listener implements EventSubscriberInterface
 
 	/**
 	 * Constructor
-	 *
-	 * @param \phpbb\language\language	$language	Language object
 	 */
 	public function __construct(\phpbb\language\language $language)
 	{
@@ -40,10 +38,8 @@ class main_listener implements EventSubscriberInterface
 
 	/**
 	 * Load common language files during user setup
-	 *
-	 * @param \phpbb\event\data	$event	Event object
 	 */
-	public function load_language_on_setup($event)
+	public function load_language_on_setup(\phpbb\event\data $event)
 	{
 		$lang_set_ext = $event['lang_set_ext'];
 		$lang_set_ext[] = [
@@ -55,10 +51,8 @@ class main_listener implements EventSubscriberInterface
 
 	/**
 	 * Replace all HTML entities within a string
-	 *
-	 * @param string	$str	String to process
 	 */
-	public function replace_html_entities($str)
+	protected function replace_html_entities(string $str)
 	{
 		$entity = <<<REGEX
 			~
@@ -81,11 +75,8 @@ class main_listener implements EventSubscriberInterface
 
 	/**
 	 * Render a string segment
-	 *
-	 * @param string	$segment	Segment to process
-	 * @param int	$idx	Index indicating whether segment should be modified
 	 */
-	public function render_segment($segment, $idx)
+	protected function render_segment(string $segment, int $idx)
 	{
 		return $idx % 2
 			// odd index: is code block or HTML tag content
@@ -96,15 +87,13 @@ class main_listener implements EventSubscriberInterface
 
 	/**
 	 * Render HTML entities
-	 *
-	 * @param \phpbb\event\data	$event	Event object
 	 */
-	public function render_html_entities($event)
+	public function render_html_entities(\phpbb\event\data $event)
 	{
 		// odd indexes are HTML tags and code blocks;
 		// even indexes are text content
 		$segments = preg_split(
-			'/(<code[^>]*>[^<]+<\/code>|<[^>]+>)/',
+			'/(<code[^>]*>[\s\S]+?<\/code>|<[^>]+>)/',
 			$event['html'],
 			-1,
 			PREG_SPLIT_DELIM_CAPTURE,
